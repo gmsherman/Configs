6024783cd583d408afa75a77f60f2eb711f5e40d
gmsherman <momolu06791@gmail.com>
2025-11-08 23:20:15 +0000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -30,3 +30,38 @@ resource "aws_instance" "gs_app_server" {
     Name = var.ec2_name
   }
 }
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "GS_EC2_security_group"
+  description = "Allow HTTP, HTTPS, and SSH access"
+  vpc_id     = "vpc-12345678"  # Specify your VPC ID
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP access from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTPS access from anywhere
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["200.10.100.0/24"]  # SSH access from specific IP range
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
