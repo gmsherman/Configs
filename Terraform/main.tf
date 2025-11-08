85bbec4d8c87575de398bcadfc70eec8acd49752
gmsherman <momolu06791@gmail.com>
2025-11-08 22:49:43 +0000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,32 @@
 
+#Terraform code for EC2 Provisioning
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+    
+    owners = ["099720109477"] # Canonical
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "gs_app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "gs-ssh"
+
+  tags = {
+    Name = var.ec2_name
+  }
+}
